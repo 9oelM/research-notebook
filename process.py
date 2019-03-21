ad2625f3fba74ff78a796feb599adb432fab61e5
9oelM <hj923@hotmail.com>
2019-03-21 15:46:33 +0000

    allow requesting in big chunks

--- process.py
+++ process.py
@@ -11,6 +11,7 @@ def toJson(response):
 
 def getIdAndType(data):
     processed = []
+    print('Total items requested: %d' %(len(data['list_items'])))
     for room in enumerate(data['list_items']):
         processed.append(
             { 'id': room[1]['simple_item']['item_id'], 
@@ -18,19 +19,40 @@ def getIdAndType(data):
             }
         )
     return processed
-    
-def getSubInfo(data):
+
+def getSubInfo(data, step):
     detailedItemURL = 'https://apis.zigbang.com/v3/items'
-    detailedItemURLPayload = {'detail':'true', 'item_ids': []}
+    detailedItemURLPayload = {'detail':'true', 'item_ids': ''}
+    # the number of data to be processed usually reaches up to ~30000 
+    # divide the requests into each with 500 items
     
     for counter, piece in enumerate(data):
-        detailedItemURLPayload['item_ids'] = '[%d]' % piece['id']
-        resp = requests.get(detailedItemURL, params=detailedItemURLPayload)
-        item = resp.json()['items'][0]['item'] # it's a single element array
-        
-        info = ['agent_lat', 'agent_lng', 'deposit', 'rent', 'manage_cost', 'floor', 'floor_all', 'building_type', 'near_subways', 'options', 'parking', 'room_type', 'size_m2']
-        for subinfo in info:
-            piece.update({
-                subinfo: item[subinfo]
-            })
-    return data
+        result = []
+        # step max < ~650
+        if ( counter != 0 and counter % step == 0) or ( counter == len(data) ):
+            print('Reached %dth counter' %counter)
+            print('Requesting')
+            
+            detailedItemURLPayload['item_ids'] = '[%s]' %detailedItemURLPayload['item_ids'][:-1]
+            
+            resp = requests.get(detailedItemURL, params=detailedItemURLPayload)
+            
+            print('Response received')
+            
+            for idx, item in enumerate(resp.json()['items']):
+                itemDetail = item['item']
+                info = ['agent_lat', 'agent_lng', 'deposit', 'rent', 'manage_cost', 'floor', 'floor_all', 'building_type', 'near_subways', 'options', 'parking', 'room_type', 'size_m2', 'id', 'is_premium']
+                
+                room = {}
+                for subinfo in info:
+                    room.update({
+                        subinfo: itemDetail[subinfo]
+                    })
+                result.append(room)
+                
+            # reset
+            detailedItemURLPayload['item_ids'] = ''
+        else:
+            detailedItemURLPayload['item_ids'] += '%d,' % piece['id']
+            
+    return result
